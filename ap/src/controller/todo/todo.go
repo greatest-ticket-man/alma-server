95ceaafc93d116191d774cad141c918d2f02f799
sunjin <yun_sunjin@cocone.co.jp>
2020-07-04 00:37:30 +0900

    todo 作成実装

--- ap/src/controller/todo/todo.go
+++ ap/src/controller/todo/todo.go
@@ -1,10 +1,14 @@
 package todo
 
 import (
+	"alma-server/ap/src/common/error/chk"
 	"alma-server/ap/src/common/util/htmlutil"
 	"alma-server/ap/src/common/util/httputil/response"
-	"net/http"
+	"alma-server/ap/src/domain/TodoService"
+	"encoding/json"
 	"html/template"
+	"net/http"
+	"time"
 )
 
 // PageHTML todoの追加画面
@@ -14,14 +18,28 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 	header := htmlutil.CreateTemplateToString("/template/common/header.html", "")
 	footer := htmlutil.CreateTemplateToString("/template/common/footer.html", "")
 
-
 	response.HTML(
 		w,
 		"/template/controller/todo/page.html",
 		map[string]interface{}{
-			"head": template.HTML(head),
+			"head":   template.HTML(head),
 			"header": template.HTML(header),
 			"footer": template.HTML(footer),
 		},
 	)
+}
+
+// CreateTodo todoの作成
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	data := &struct {
+		Title string `json:"title"`
+		Desc  string `json:"desc"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(data)
+	chk.SE(err)
+
+	// CreateTodo
+	TodoService.CreateTodo(r.Context(), time.Now(), data.Title, data.Desc, "sunjin")
+
+	response.JSON(w, true)
 }
---
+++ ap/src/domain/TodoService/TodoService.go
@@ -0,0 +1,22 @@
+package TodoService
+
+import (
+	"alma-server/ap/src/repository/user/UserTodoRepository"
+	"context"
+	"time"
+)
+
+// CreateTodo todoを作成する
+func CreateTodo(ctx context.Context, txTime time.Time, title string, desc string, name string) bool {
+
+	todo := &UserTodoRepository.USER_TODO{
+		Title:        title,
+		Name:         name,
+		Desc:         desc,
+		CreateTime:   txTime,
+		UpdateTime:   txTime,
+		DeadlineTime: txTime.AddDate(0, 3, 0),
+	}
+
+	return UserTodoRepository.Insert(ctx, todo)
+}
