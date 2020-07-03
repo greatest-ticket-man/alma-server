c3597d89f355985684bb902a122b75e70e044cb0
sunjin <yun_sunjin@cocone.co.jp>
2020-07-03 21:33:24 +0900

    js

---
+++ ap/asset/static/js/request.js
@@ -0,0 +1,43 @@
+window.Alma = window.Alma || {};
+(function(_Alma) {
+
+    class Req {
+        async post(url, data, options = { reload: true }) {
+            try {
+                const response = await fetch(url, data);
+                const json = await response.json();
+                if (response.status == 200) {
+                    console.log(`通信に成功しました: ${JSON.stringify(json)}`);
+                    if (options.reload === true) {
+                        setTimeout(() => location.reload(), 2000);
+                    }
+                } 
+                if (response.status === 500) {
+                    console.log(json);
+                } else if (response.status !== 200) {
+                    console.log(json);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        createPostData(data = {}, cache = 'no-cache') {
+            return {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, cors, *same-origin
+                cache: cache, // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, same-origin, *omit
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'no-referrer', // no-referrer, *client
+                body: JSON.stringify(data), // 本文のデータ型は 'Content-Type' ヘッダーと一致する必要があります
+            };
+        }
+    }
+
+
+})(window.Alma);
