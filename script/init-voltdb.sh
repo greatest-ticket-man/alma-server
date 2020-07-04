681b65f4f823a2dbf69e4e43d3fdebeae04325c0
sunjin <sunjin110@gmail.com>
2020-07-04 17:34:43 +0900

    script景

---
+++ script/init-voltdb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# ubuntu
+sudo apt update
+sudo apt -y install ant 
+sudo apt -y install cmake
+sudo apt -y install python
+sudo apt -y install ccache
+
+# VoltDB 9.3.1
+rm -rf /tmp/voltdb
+
+cd /tmp
+git clone https://github.com/VoltDB/voltdb.git
+cd /tmp/voltdb
+git checkout refs/tags/voltdb-9.3.1
+ant -Djmemcheck=NO_MEMCHECK
+
+cd /tmp
+rm -rf /usr/local/voltdb
+sudo mv ./voltdb /usr/local/
+
+# push path
+echo 'export PATH=$PATH:/usr/local/voltdb/bin' >> $HOME/.bashrc
+
+source $HOEM/.bashrc
+
+voltdb init --dir $HOME/voltdb_data
---
+++ script/local-mariadb-start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# server
+mysql.server start
