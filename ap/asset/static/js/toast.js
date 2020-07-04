b837b0609a7fd16c11bf9b914198e4d509abd931
sunjin <yun_sunjin@cocone.co.jp>
2020-07-04 13:21:41 +0900

    ボタンが出るようになった！

---
+++ ap/asset/static/js/toast.js
@@ -0,0 +1,89 @@
+window.Alma = window.Alma || {};
+(function(_Alma) {
+
+    class Toast {
+
+
+        // toastを作成する
+        createToast() {
+            
+            // 要素作成
+            let div = document.createElement('div');
+            div.style = 'position: absolute; top: 5vh; right: 50vw;';
+
+            let toastDiv = document.createElement('div');
+            toastDiv.classList.add('toast', 'bg-info', 'text-white');
+            toastDiv.setAttribute('role', 'alert');
+            toastDiv.setAttribute('aria-live', 'assertive');
+            toastDiv.setAttribute('aria-atomic', 'true');
+
+            let toastHeaderDiv = document.createElement('div');
+            toastHeaderDiv.classList.add('toast-header', 'bg-info', 'text-white');
+            
+            let toastHeaderIcon = document.createElement('i');
+            toastHeaderIcon.classList.add('fas', 'fa-info-circle', 'fa-lg', 'mr-2');
+
+            let toastHeaderTitle = document.createElement('strong');
+            toastHeaderTitle.className = 'mr-auto';
+            toastHeaderTitle.innerText = 'Info';
+
+            let toastHeaderDesc = document.createElement('small');
+            toastHeaderDesc.innerText = '何分前';
+
+            let toastButton = document.createElement('button');
+            toastButton.classList.add('ml-2', 'my-1', 'close', 'text-white');
+            toastButton.setAttribute('data-dismiss', 'toast');
+            toastButton.setAttribute('aria-label', 'Close');
+
+            let toastButtonSpan = document.createElement('span');
+            toastButtonSpan.setAttribute('aria-hidden', 'true');
+            toastButtonSpan.innerHTML = '×';
+
+
+            let toastBody = document.createElement('div');
+            toastBody.className = 'toast-body';
+            toastBody.innerText = 'あれあれがそれそれ';
+
+
+            // 要素組み立て
+            
+            // button
+            toastButton.appendChild(toastButtonSpan);
+
+            // header
+            toastHeaderDiv.appendChild(toastHeaderIcon);
+            toastHeaderDiv.appendChild(toastHeaderTitle);
+            toastHeaderDiv.appendChild(toastHeaderDesc);
+            toastHeaderDiv.appendChild(toastButton);
+
+            // div
+            toastDiv.appendChild(toastHeaderDiv);
+            toastDiv.appendChild(toastBody);
+
+
+            // 初期化
+            let option = {
+                animation: true,
+                autohide: true,
+                delay: 2000,
+            };
+
+            let toast = new bootstrap.Toast(toastDiv, option);
+
+            // toastDiv
+
+            div.appendChild(toastDiv);
+
+            // add body
+            document.body.appendChild(div);
+
+            // show
+            toast.show();
+        }
+    }
+
+
+    _Alma.toast = new Toast();
+
+
+})(window.Alma);
