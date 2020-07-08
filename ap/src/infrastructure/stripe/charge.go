8aff324983b88944867d7647b323a3c9db721bd2
sunjin <yun_sunjin@cocone.co.jp>
2020-07-08 13:15:17 +0900

    aaa

--- ap/src/infrastructure/stripe/charge.go
+++ ap/src/infrastructure/stripe/charge.go
@@ -1,10 +1,27 @@
 package stripe
 
+import (
+	"alma-server/ap/src/common/error/chk"
+
+	stripego "github.com/stripe/stripe-go/v71"
+)
+
 // carge 支払い
 
 // document https://stripe.com/docs/api/charges
 
 // CreateCharge charge
-func (c *AlmaStripe) CreateCharge() {
+// token is https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+func (c *AlmaStripe) CreateCharge(amount int64, currency stripego.Currency, desc string, token string) *stripego.Charge {
 
+	params := &stripego.ChargeParams{
+		Amount:      stripego.Int64(amount),
+		Currency:    stripego.String(string(currency)),
+		Description: stripego.String(desc),
+		Source:      &stripego.SourceParams{Token: stripego.String(token)},
+	}
+
+	charge, err := c.client.Charges.New(params)
+	chk.BE(err)
+	return charge
 }
