e409709fc648ab48d36580033c0949201f0bba78
sunjin <yun_sunjin@cocone.co.jp>
2020-07-19 23:13:46 +0900

    loginform中断

--- ap/src/controller/login/login.go
+++ ap/src/controller/login/login.go
@@ -1,22 +1,21 @@
 package login
 
 import (
-	"alma-server/ap/src/common/util/htmlutil"
 	"alma-server/ap/src/common/util/httputil/response"
-	"html/template"
+	"alma-server/ap/src/domain/CommonHTMLService"
 	"net/http"
 )
 
 // PageHTML ログイン画面
 func PageHTML(w http.ResponseWriter, r *http.Request) {
 
-	head := htmlutil.CreateTemplateToString("/template/common/head.html", "")
-
 	response.HTML(
 		w,
 		"/template/controller/login/page.html",
 		map[string]interface{}{
-			"head": template.HTML(head), // template.HTML型に変換すれば、エスケープされなくなる
+			"head":   CommonHTMLService.GetHead(),
+			"header": CommonHTMLService.GetHeader(),
+			"footer": CommonHTMLService.GetFooter(),
 		},
 	)
 }
