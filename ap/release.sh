7e44796a16c0e53bfa4ab166dd0d803eadc8d3e0
sunjin <yun_sunjin@cocone.co.jp>
2020-07-19 10:28:07 +0900

    a

--- ap/release.sh
+++ ap/release.sh
@@ -4,9 +4,19 @@
 echo "build"
 GOOS=linux GOARCH=arm go build
 
+# kill
+ssh -t www@221.170.118.102 'sudo ./stop-alma.sh'
+
 # バイナリ
-scp -p ./ap www@192.168.2.201:/home/www
+scp -p ./ap www@221.170.118.102:/home/www
 
 # configとasset
-scp -pr ./asset www@192.168.2.201:/home/www
-scp -pr ./config www@192.168.2.201:/home/www
+scp -pr ./asset www@221.170.118.102:/home/www
+scp -pr ./config www@221.170.118.102:/home/www
+
+# start
+# ssh -t www@221.170.118.102 'sudo ./start-alma.sh'
+echo "直接Serverでstartは実行する必要があります"
+
+# バイナリを削除
+rm ap
