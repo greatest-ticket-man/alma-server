b87df6d235b9c866c4cfeaa73bb99f4b099bfe58
sunjin <sunjin110@gmail.com>
2020-07-23 16:02:09 +0900

    databinator

---
+++ ap/asset/static/js/common/databinator.js
@@ -0,0 +1,73 @@
+/**
+ *
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * Usage:
+ * const detabinator = new Detabinator(element);
+ * detabinator.inert = true;  // Sets all focusable children of element to tabindex=-1
+ * detabinator.inert = false; // Restores all focusable children of element
+ * Limitations: Doesn't support Shadow DOM v0 :P
+ */
+
+class Databinator {
+    constructor(element) {
+        if (!element) {
+            throw new Error('Missing required argument. new Detabinator needs an element reference\n必要な引数がありません。 新しいDetabinatorには要素参照が必要です');
+        }
+
+        this._insert = false;
+        this._focusableEmenentsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
+        this._focusableEmenents = Array.from(
+            element.querySelectorAll(this._focusableEmenentsString)
+        );
+    }
+
+    get insert() {
+        return this._insert;
+    }
+
+    set insert(isInsert) {
+        if (this._insert === isInsert) {
+            return;
+        }
+
+        this._insert = isInsert;
+
+        this._focusableEmenents.forEach((child) => {
+            if (isInsert) {
+                // If the child has an explict tabindex save it
+                if (child.hasAttribute('tabindex')) {
+                    child.__savedTabindex = child.tabIndex;
+                }
+
+                // Set ALL focusable children to tabindex -1
+                child.setAttribute('tabindex', -1);
+            } else {
+                // If the child has a saved tabindex, restore it
+                // Because the value could be 0, explicitly check that it's not false
+
+                if (child.__savedTabindex === 0 || child.__savedTabindex) {
+                    return child.setAttribute('tabindex', child.__savedTabindex);
+                } else {
+                    // Remove tabindex from ANY REMAINING children
+                    child.removeAttribute('tabindex');
+                }
+            }
+        });
+    }
+}
