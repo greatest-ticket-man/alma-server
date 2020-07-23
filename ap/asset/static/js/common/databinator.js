e94b0a10e9d2ace73489374404f9b525f11dabf5
sunjin <yun_sunjin@cocone.co.jp>
2020-07-23 22:18:16 +0900

    中断

--- ap/asset/static/js/common/databinator.js
+++ ap/asset/static/js/common/databinator.js
@@ -1,3 +1,50 @@
+// class Detabinator {
+//     constructor(element) {
+//         if (!element) {
+//             throw new Error('Missing required argument. new Detabinator needs an element reference');
+//         }
+//         this._inert = false;
+//         this._focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
+//         this._focusableElements = Array.from(
+//             element.querySelectorAll(this._focusableElementsString)
+//         );
+//     }
+
+//     get inert() {
+//         return this._inert;
+//     }
+
+//     set inert(isInert) {
+//         if (this._inert === isInert) {
+//             return;
+//         }
+
+//         this._inert = isInert;
+
+//         this._focusableElements.forEach((child) => {
+//             if (isInert) {
+//                 // If the child has an explict tabindex save it
+//                 if (child.hasAttribute('tabindex')) {
+//                     child.__savedTabindex = child.tabIndex;
+//                 }
+//                 // Set ALL focusable children to tabindex -1
+//                 child.setAttribute('tabindex', -1);
+//             } else {
+//                 // If the child has a saved tabindex, restore it
+//                 // Because the value could be 0, explicitly check that it's not false
+//                 if (child.__savedTabindex === 0 || child.__savedTabindex) {
+//                     return child.setAttribute('tabindex', child.__savedTabindex);
+//                 } else {
+//                     // Remove tabindex from ANY REMAINING children
+//                     child.removeAttribute('tabindex');
+//                 }
+//             }
+//         });
+//     }
+// }
+
+
+
 /**
  *
  * Copyright 2016 Google Inc. All rights reserved.
@@ -24,32 +71,32 @@
  * Limitations: Doesn't support Shadow DOM v0 :P
  */
 
-class Databinator {
+class Detabinator {
     constructor(element) {
         if (!element) {
             throw new Error('Missing required argument. new Detabinator needs an element reference\n必要な引数がありません。 新しいDetabinatorには要素参照が必要です');
         }
 
-        this._insert = false;
+        this._inert = false;
         this._focusableEmenentsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
         this._focusableEmenents = Array.from(
             element.querySelectorAll(this._focusableEmenentsString)
         );
     }
 
-    get insert() {
-        return this._insert;
+    get inert() {
+        return this._inert;
     }
 
-    set insert(isInsert) {
-        if (this._insert === isInsert) {
+    set inert(isInert) {
+        if (this._inert === isInert) {
             return;
         }
 
-        this._insert = isInsert;
+        this._inert = isInert;
 
         this._focusableEmenents.forEach((child) => {
-            if (isInsert) {
+            if (isInert) {
                 // If the child has an explict tabindex save it
                 if (child.hasAttribute('tabindex')) {
                     child.__savedTabindex = child.tabIndex;
