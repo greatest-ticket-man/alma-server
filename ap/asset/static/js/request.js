ed5b98196499f5ecb017dd36a8bf7484dfaa2eb8
sunjin <sunjin110@gmail.com>
2020-08-02 18:13:35 +0900

    中断

--- ap/asset/static/js/request.js
+++ ap/asset/static/js/request.js
@@ -2,26 +2,51 @@ window.Alma = window.Alma || {};
 (function(_Alma) {
 
     class Req {
+        // async post(url, data, options = { reload: true }) {
+
+        //     try {
+        //         const response = await fetch(url, data);
+        //         const json = await response.json();
+        //         if (response.status == 200) {
+        //             console.log(`通信に成功しました: ${JSON.stringify(json)}`);
+        //             if (options.reload === true) {
+        //                 window.Alma.toast.success('成功しました');
+        //                 setTimeout(() => location.reload(), 2000);
+        //             }
+        //             return json;
+        //         } 
+        //         if (response.status === 500) {
+        //             window.Alma.toast.error('失敗しました...');
+        //             console.log(json);
+        //         } else if (response.status !== 200) {
+        //             window.Alma.toast.error('失敗しました...');
+        //             console.log(json);
+        //         }
+        //     } catch (e) {
+        //         console.log("error====");
+        //         console.log(e);
+        //     }
+        // }
+
         async post(url, data, options = { reload: true }) {
 
             try {
                 const response = await fetch(url, data);
                 const json = await response.json();
-                if (response.status == 200) {
+                console.log("json is ", json);
+                if (json.success) {
                     console.log(`通信に成功しました: ${JSON.stringify(json)}`);
                     if (options.reload === true) {
                         window.Alma.toast.success('成功しました');
                         setTimeout(() => location.reload(), 2000);
                     }
                     return json;
-                } 
-                if (response.status === 500) {
-                    window.Alma.toast.error('失敗しました...');
+                }  else {
+                    window.Alma.toast.error("失敗しました");
                     console.log(json);
-                } else if (response.status !== 200) {
-                    window.Alma.toast.error('失敗しました...');
-                    console.log(json);
+                    return json;
                 }
+
             } catch (e) {
                 console.log(e);
             }
