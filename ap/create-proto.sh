76113ca4ffeebbb9cd354301d10fe320dd801c16
sunjin <sunjin110@gmail.com>
2020-08-02 16:42:01 +0900

    a

--- ap/create-proto.sh
+++ ap/create-proto.sh
@@ -4,11 +4,13 @@ DIR=`pwd`
 cd ../..
 OUT_DIR=`pwd`
 
+
+
 # clear
 rm -rf $DIR/src/infrastructure/grpc/proto
 mkdir -p $DIR/src/infrastructure/grpc/proto
 
 export PATH="$PATH:$HOME/go/bin"
 
-cd proto
+cd $DIR/proto
 find . name "*.proto" -exec protoc --proto_path . --go_out=plugins=grpc:$OUT_DIR {} \;
