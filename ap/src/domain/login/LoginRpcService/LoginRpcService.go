7bc0d6e414d76b9119ffc939a5c7acba884b4d29
sunjin <sunjin110@gmail.com>
2020-08-02 17:12:34 +0900

    protoを導入

--- ap/src/domain/login/LoginRpcService/LoginRpcService.go
+++ ap/src/domain/login/LoginRpcService/LoginRpcService.go
@@ -1,11 +1,18 @@
 package LoginRpcService
 
-import "context"
+import (
+	"alma-server/ap/src/infrastructure/grpc/proto/login"
+	"context"
+	"log"
+	"time"
+)
 
 // Login ログイン処理
-func Login(ctx context.Context, txTime time.Time, email string, password string) {
+func Login(ctx context.Context, txTime time.Time, email string, password string) *login.LoginReply {
 
+	log.Println("email is ", email, "pass is ", password)
 
-
-
+	return &login.LoginReply{
+		Token: "hoge",
+	}
 }
