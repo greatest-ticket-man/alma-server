40087d51d2b07f470054a328ee84557973dbe7c2
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-08-09 20:12:21 +0900

    errhandling

--- ap/src/domain/signup/SignupRpcService/SignupRpcService.go
+++ ap/src/domain/signup/SignupRpcService/SignupRpcService.go
@@ -1,16 +1,30 @@
 package SignupRpcService
 
 import (
+	"alma-server/ap/src/common/error/chk"
+	"alma-server/ap/src/common/error/errmsg"
+	"alma-server/ap/src/common/util/uniqueidutil"
+	"alma-server/ap/src/repository/user/UserAccountRepository"
 	"context"
-	"log"
 	"time"
 )
 
 // Signup サインアップ
 func Signup(ctx context.Context, txTime time.Time, email string, password string) bool {
 
-	log.Println("signup")
-	log.Println("email is ", email, "pass is ", password)
+	// 先に、指定されたemailがすでに存在するかどうかを確認する
+	userAccount := UserAccountRepository.GetFromEmail(ctx, email)
+	if userAccount != nil {
+		// このメールアドレスはすでに使用されています
+		chk.LE(errmsg.SignupAlreadyUseEmail)
+	}
 
+	// 作成
+	UserAccountRepository.Insert(ctx,
+		txTime,
+		uniqueidutil.GenerateUniqueID(),
+		email,
+		password,
+	)
 	return true
 }
