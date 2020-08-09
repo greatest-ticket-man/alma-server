2657f4edb8fbc154dac11af7a83c7a14d37df7a7
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-08-09 20:03:20 +0900

    signup

--- ap/asset/static/js/controller/signup/signup.js
+++ ap/asset/static/js/controller/signup/signup.js
@@ -3,7 +3,37 @@
 class Signup {
     constructor() {
 
-        
+        this.signupEmailInputEl = document.getElementById('js-email');
+        this.signupPassInputEl = document.getElementById('js-password');
+
+        this.signupButtonEl = document.querySelector('.js-signup');
+
+        this.signup = this.signup.bind(this);
+
+        this.addEventListener();
+    }
+
+    addEventListener() {
+
+        console.log("b el is ", this.signupButtonEl);
+
+        this.signupButtonEl.addEventListener('click', this.signup);
+    }
+
+    // signup
+    async signup() {
+
+        const email = this.signupEmailInputEl.value;
+        const pass = CybozuLabs.MD5.calc(this.signupPassInputEl.value);
+
+        const data = {
+            email: email,
+            password: pass,
+        };
+
+        let response = await window.Alma.req.post('/signup', window.Alma.req.createPostData(data), {reload: false});
+
+        console.log('response is ', response);
     }
 
 
