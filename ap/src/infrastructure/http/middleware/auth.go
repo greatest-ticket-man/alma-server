2e768992f81dfc45c220e9987481496db373e5d2
sunjin <yun_sunjin@cocone.co.jp>
2020-08-10 17:24:51 +0900

    中断

---
+++ ap/src/infrastructure/http/middleware/auth.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+// AuthMiddleware 認証
+func AuthMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+	// token := jwt.Auth(r)
+
+	// もし、認証が合わないとか、有効期限が切れたとかなったら、callbackのパスをつけてlogin画面に遷移させる
+
+	// TODO 認証がとおった場合は新しいTokenを送信する
+
+	log.Println("============== auth !!! ================")
+
+	next(w, r)
+
+}
