46d71f8d86a7c509daba58338a22d23b08648b15
sunjin <yun_sunjin@cocone.co.jp>
2020-08-11 09:35:11 +0900

    create page

---
+++ ap/src/controller/event/create/create.go
@@ -0,0 +1,22 @@
+package create
+
+import (
+	"alma-server/ap/src/common/util/htmlutil"
+	"alma-server/ap/src/common/util/httputil/response"
+	"html/template"
+	"net/http"
+)
+
+// PageHTML イベント作成画面
+func PageHTML(w http.ResponseWriter, r *http.Request) {
+	response.HTML(
+		w,
+		"/template/component/base.html",
+		map[string]interface{}{
+			"mainTitle":   "イベントの作成",
+			"mainContent": template.HTML(htmlutil.CreateTemplateToString("/template/controller/event/create/content.html", "")),
+			"script":      "",
+			"css":         "",
+		},
+	)
+}
