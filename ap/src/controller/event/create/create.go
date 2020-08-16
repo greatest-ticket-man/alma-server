108ac1a0f5bc408f47528af245c8ad8786c88eb1
sunjin <yun_sunjin@cocone.co.jp>
2020-08-16 11:14:37 +0900

    a

--- ap/src/controller/event/create/create.go
+++ ap/src/controller/event/create/create.go
@@ -1,8 +1,13 @@
 package create
 
 import (
+	"alma-server/ap/src/common/almactx"
+	"alma-server/ap/src/common/error/chk"
 	"alma-server/ap/src/common/util/htmlutil"
 	"alma-server/ap/src/common/util/httputil/response"
+	"alma-server/ap/src/domain/event/EventRpcService"
+	"alma-server/ap/src/infrastructure/grpc/proto/event"
+	"encoding/json"
 	"html/template"
 	"net/http"
 )
@@ -19,4 +24,24 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 			"css":         template.HTML(htmlutil.CreateTemplateToString("/template/controller/event/create/css.html", "")),
 		},
 	)
+}
+
+// Event イベントの作成
+func Event(w http.ResponseWriter, r *http.Request) {
+
+	// param
+	req := &event.CreateEventRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	chk.SE(err)
+
+	ctx := r.Context()
+
+	mid := almactx.GetMid(ctx)
+	txTime := almactx.GetTxTime(ctx)
+
+	// create event
+	reply := EventRpcService.CreateEvent(ctx, mid, txTime, req.EventName, req.OrganizationName, req.MemberInfoList)
+
+	// response
+	response.JSON(w, reply)
 }
