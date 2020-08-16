eda1435c141ba90dff7282aee29bdeb37ce697e5
sunjin <yun_sunjin@cocone.co.jp>
2020-08-16 13:22:37 +0900

    event_id

---
+++ ap/asset/static/js/localstorage.js
@@ -0,0 +1,46 @@
+window.Alma = window.Alma || {};
+(function(_Alma) {
+
+    class LocalStorage {
+
+        constructor() {
+            this.event_id = 'event_id';
+        }
+
+        /**
+         * セット
+         * @param {string} key 
+         * @param {string} value 
+         */
+        set(key, value) {
+            localStorage.setItem(key, value);
+        }
+
+        /**
+         * 取得
+         * @param {string} key
+         * @returns {string} value 
+         */
+        get(key) {
+            return localStorage.getItem(key);
+        }
+
+        /**
+         * ローカルストレージを削除する
+         */
+        clear() {
+            localStorage.clear();
+        }
+
+        /**
+         * 削除
+         * @param {string} key 
+         */
+        remove(key) {
+            localStorage.removeItem(key);
+        }
+
+    }   
+
+    _Alma.localStorage = new LocalStorage();
+})(window.Alma);
