6f9d2e378c05b18b3f219e63458b3fb9d5b85ac5
sunjin <yun_sunjin@cocone.co.jp>
2020-08-16 13:22:37 +0900

    event_id

--- ap/src/repository/user/event/UserEventRepository/UserEventRepository.go
+++ ap/src/repository/user/event/UserEventRepository/UserEventRepository.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"reflect"
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // イベントの情報リポジトリ
@@ -19,13 +17,13 @@ var reflectType = reflect.TypeOf(&UserEvent{})
 
 // UserEvent .
 type UserEvent struct {
-	ID            *primitive.ObjectID `bson:"_id,omitempty"` // ObjectID
-	Name          string              `bson:"name"`
-	Organization  string              `bson:"organization"`
-	MemberMap     map[string]string   `bson:"members"`
-	TempMemberMap map[string]string   `bson:"tempmembers"`
-	CreateTime    time.Time           `bson:"ct"`
-	UpdateTime    time.Time           `bson:"ut"`
+	ID            string            `bson:"_id,omitempty"`
+	Name          string            `bson:"name"`
+	Organization  string            `bson:"organization"`
+	MemberMap     map[string]string `bson:"members"`
+	TempMemberMap map[string]string `bson:"tempmembers"`
+	CreateTime    time.Time         `bson:"ct"`
+	UpdateTime    time.Time         `bson:"ut"`
 }
 
 func getDb(ctx context.Context) *mongodb.AlmaCollection {
@@ -33,15 +31,15 @@ func getDb(ctx context.Context) *mongodb.AlmaCollection {
 }
 
 // Insert イベントの作成
-func Insert(ctx context.Context, txTime time.Time, name string, organization string, memberMap map[string]string, tempMemberMap map[string]string) *primitive.ObjectID {
+func Insert(ctx context.Context, txTime time.Time, eventID string, name string, organization string, memberMap map[string]string, tempMemberMap map[string]string) interface{} {
 
 	userEvent := &UserEvent{
+		ID:           eventID,
 		Name:         name,
 		Organization: organization,
 		CreateTime:   txTime,
 		UpdateTime:   txTime,
 	}
 
-	result := getDb(ctx).InsertOne(userEvent).(primitive.ObjectID)
-	return &result
+	return getDb(ctx).InsertOne(userEvent)
 }
