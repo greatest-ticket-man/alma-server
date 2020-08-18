05df17ceff54c1a371a5cbb1098860af41dfdb99
sunjin <yun_sunjin@cocone.co.jp>
2020-08-19 00:03:24 +0900

    event id pathの挙動修正

--- ap/src/controller/event/event.go
+++ ap/src/controller/event/event.go
@@ -2,13 +2,13 @@ package event
 
 import (
 	"alma-server/ap/src/common/almactx"
+	"alma-server/ap/src/common/error/chk"
 	"alma-server/ap/src/common/util/htmlutil"
 	"alma-server/ap/src/common/util/httputil/response"
-	"alma-server/ap/src/common/util/jsonutil"
 	"alma-server/ap/src/domain/event/EventRpcService"
 	"alma-server/ap/src/infrastructure/grpc/proto/common"
+	"encoding/json"
 	"html/template"
-	"log"
 	"net/http"
 )
 
@@ -20,22 +20,16 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 		Event: r.FormValue("event"),
 	}
 
-	// TODO req.EventRequest なければErrorとかメッセージを送る
-
-	log.Println("req is ", jsonutil.Marshal(req))
-
 	ctx := r.Context()
 	mid := almactx.GetMid(ctx)
 
 	result := EventRpcService.GetEvent(ctx, mid, req.Event)
 
-	log.Println("result is ", result)
-
 	response.HTML(
 		w,
 		"/template/component/base.html",
 		map[string]interface{}{
-			"mainTitle": "公演情報",
+			"mainTitle": "イベント情報",
 			"mainContent": template.HTML(htmlutil.CreateTemplateToString("/template/controller/event/content.html",
 				map[string]interface{}{
 					"eventId":   result.EventId,
@@ -45,4 +39,20 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 			"css":    template.HTML(htmlutil.CreateTemplateToString("/template/controller/event/css.html", "")),
 		},
 	)
+}
+
+// GetEvent イベント名を取得する
+func GetEvent(w http.ResponseWriter, r *http.Request) {
+
+	// param
+	req := &common.EventRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	chk.SE(err)
+
+	ctx := r.Context()
+
+	mid := almactx.GetMid(ctx)
+
+	result := EventRpcService.GetEvent(ctx, mid, req.Event)
+	response.JSON(w, result)
 }
