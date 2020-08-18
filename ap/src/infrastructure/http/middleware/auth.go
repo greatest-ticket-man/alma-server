fedee6b1402985f5934805b8e8a65f796f299e6d
sunjin <yun_sunjin@cocone.co.jp>
2020-08-18 21:32:41 +0900

    auth

--- ap/src/infrastructure/http/middleware/auth.go
+++ ap/src/infrastructure/http/middleware/auth.go
@@ -4,7 +4,11 @@ import (
 	"alma-server/ap/src/common/almactx"
 	"alma-server/ap/src/common/jwt"
 	"alma-server/ap/src/common/util/cookieutil"
+	"alma-server/ap/src/common/util/httputil/response"
+	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -14,22 +18,26 @@ func AuthMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFun
 	// cookieからtokenを取得
 	tokenStr := cookieutil.GetCookie(r, "token")
 
-	token, _ := jwt.Parse(tokenStr)
+	txTime := time.Now()
+
+	// cookieutil.DeleteCookie(w, r, txTime, "token")
+
+	token, err := jwt.Parse(tokenStr)
 	// TODO どうにかする
-	// if err != nil || !token.Valid {
+	if err != nil || !token.Valid {
 
-	// 	log.Println("redirect...")
-	// 	log.Println("err is ", err)
+		log.Println("redirect...")
+		log.Println("err is ", err)
 
-	// 	// redirect
-	// 	response.RedirectHTML(w, r, fmt.Sprintf("/login?fallback=%s", url.QueryEscape(r.RequestURI)))
-	// 	return
-	// }
+		// redirect
+		response.RedirectHTML(w, r, fmt.Sprintf("/login?fallback=%s", url.QueryEscape(r.RequestURI)))
+		return
+	}
 
 	// commonDataを追加する
 	claimMap := jwt.GetClaimMap(token)
 	commonData := &almactx.CommonData{
-		TxTime: time.Now(),
+		TxTime: txTime,
 		Mid:    claimMap["mid"].(string),
 		Email:  claimMap["email"].(string),
 	}
@@ -37,7 +45,7 @@ func AuthMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFun
 	ctx := almactx.WithData(r.Context(), commonData)
 
 	// 新しいtokenをセットする(refresh token)
-	cookieutil.SetCookie(w, commonData.TxTime, "token", jwt.New(commonData.TxTime, commonData.Mid, commonData.Email))
+	// cookieutil.SetCookie(w, commonData.TxTime, "token", jwt.New(commonData.TxTime, commonData.Mid, commonData.Email))
 
 	next(w, r.WithContext(ctx))
 }
