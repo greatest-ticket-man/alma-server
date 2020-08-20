2126a0dde69ff29d0991c34aa3dee19edc6b4afb
sunjin <yun_sunjin@cocone.co.jp>
2020-08-21 00:53:09 +0900

    base htmlに変換中

--- ap/src/controller/home/dashboard/dashboard.go
+++ ap/src/controller/home/dashboard/dashboard.go
@@ -1,12 +1,10 @@
 package dashboard
 
 import (
-	"alma-server/ap/src/common/util/htmlutil"
 	"alma-server/ap/src/common/util/httputil/param"
 	"alma-server/ap/src/common/util/httputil/response"
 	"alma-server/ap/src/domain/event/EventService"
 	"alma-server/ap/src/repository/user/event/UserEventRepository"
-	"html/template"
 	"net/http"
 )
 
@@ -17,6 +15,7 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 	// なにも指定されていない場合は、Emptyにリダイレクト
 	if eventID == "" {
 		response.RedirectHTML(w, r, "/home/dashboard/empty")
+		return
 	}
 
 	userEvent := EventService.GetEvent(r.Context(), eventID)
@@ -25,31 +24,27 @@ func PageHTML(w http.ResponseWriter, r *http.Request) {
 		userEvent = &UserEventRepository.UserEvent{Name: "TODO このパターンはエラーメッセージを出すようにする"}
 	}
 
-	response.HTML(
+	response.BaseHTML(
 		w,
-		"/template/component/base.html",
-		map[string]interface{}{
-			"mainTitle":   "ダッシュボード",
-			"mainContent": template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/content.html", "")),
-			"script":      template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/script.html", "")),
-			"css":         template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/css.html", "")),
-			"eventName":   userEvent.Name,
-		},
+		"ダッシュボード",
+		"/template/controller/home/dashboard/content.html",
+		nil,
+		"/template/controller/home/dashboard/script.html",
+		"/template/controller/home/dashboard/css.html",
+		userEvent.Name,
 	)
 }
 
 // PageHTMLEmpty イベントが選択されていない場合はここにRedirect
 func PageHTMLEmpty(w http.ResponseWriter, r *http.Request) {
 
-	response.HTML(
+	response.BaseHTML(
 		w,
-		"/template/component/base.html",
-		map[string]interface{}{
-			"mainTitle":   "ダッシュボード",
-			"mainContent": template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/empty/content.html", "")),
-			"script":      template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/empty/script.html", "")),
-			"css":         template.HTML(htmlutil.CreateTemplateToString("/template/controller/home/dashboard/empty/css.html", "")),
-			"eventName":   EventService.DefaultEventName,
-		},
+		"ダッシュボード",
+		"/template/controller/home/dashboard/empty/content.html",
+		nil,
+		"/template/controller/home/dashboard/empty/script.html",
+		"/template/controller/home/dashboard/empty/css.html",
+		"",
 	)
 }
