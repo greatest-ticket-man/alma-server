da78034f1a1ed71da522fab8b2e9fe8fab9927d0
sunjin <yun_sunjin@cocone.co.jp>
2020-08-21 19:03:39 +0900

    titleを押したらダッシュボードに遷移するように更新

--- ap/asset/static/js/common/base.js
+++ ap/asset/static/js/common/base.js
@@ -1,4 +1,25 @@
-// 'use strict';
+'use strict';
+
+class Base {
+    constructor() {
+        this.baseHeaderTitleEl = document.querySelector('.js-base-header-title');
+
+        this.addEventListener();
+    }
+
+    addEventListener() {
+        this.baseHeaderTitleEl.addEventListener('click', this.goDashboardPage);
+    }
+
+    goDashboardPage() {
+
+        console.log("hoge");
+        // 遷移
+        window.Alma.location.href(window.Alma.location.home_dashboard);
+    }
+}
+
+new Base();
 
 // class Base {
 //     constructor() {
