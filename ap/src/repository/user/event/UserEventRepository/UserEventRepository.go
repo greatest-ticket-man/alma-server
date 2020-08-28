f1c6692567b7cfa31431031c617fad063db1e2cc
sunjin <yun_sunjin@cocone.co.jp>
2020-08-28 10:05:30 +0900

    中断

--- ap/src/repository/user/event/UserEventRepository/UserEventRepository.go
+++ ap/src/repository/user/event/UserEventRepository/UserEventRepository.go
@@ -33,29 +33,11 @@ var reflectType = reflect.TypeOf((*UserEvent)(nil))
 
 // UserEvent .
 type UserEvent struct {
-	ID             string            `bson:"_id,omitempty"`
-	Name           string            `bson:"name"`
-	Organization   string            `bson:"organization"`
-	MemberList     []*MemberInfo     `bson:"members"`
-	TempMemberList []*TempMemberInfo `bson:"tmps"`
-	CreateTime     time.Time         `bson:"ct"`
-	UpdateTime     time.Time         `bson:"ut"`
-}
-
-// MemberInfo .
-type MemberInfo struct {
-	Mid        string    `bson:"mid"`
-	AuthID     string    `bson:"auth"`
-	CreateTime time.Time `bson:"ct"`
-	UpdateTime time.Time `bson:"ut"`
-}
-
-// TempMemberInfo .
-type TempMemberInfo struct {
-	Email      string    `bson:"email"`
-	AuthID     string    `bson:"auth"`
-	CreateTime time.Time `bson:"ct"`
-	UpdateTime time.Time `bson:"ut"`
+	ID           string    `bson:"_id,omitempty"`
+	Name         string    `bson:"name"`
+	Organization string    `bson:"organization"`
+	CreateTime   time.Time `bson:"ct"`
+	UpdateTime   time.Time `bson:"ut"`
 }
 
 func getDb(ctx context.Context) *mongodb.AlmaCollection {
@@ -63,23 +45,21 @@ func getDb(ctx context.Context) *mongodb.AlmaCollection {
 }
 
 // Insert イベントの作成
-func Insert(ctx context.Context, txTime time.Time, eventID string, name string, organization string, memberList []*MemberInfo, tempMemberList []*TempMemberInfo) interface{} {
+func Insert(ctx context.Context, txTime time.Time, eventID string, name string, organization string) interface{} {
 
 	userEvent := &UserEvent{
-		ID:             eventID,
-		Name:           name,
-		Organization:   organization,
-		MemberList:     memberList,
-		TempMemberList: tempMemberList,
-		CreateTime:     txTime,
-		UpdateTime:     txTime,
+		ID:           eventID,
+		Name:         name,
+		Organization: organization,
+		CreateTime:   txTime,
+		UpdateTime:   txTime,
 	}
 
 	return getDb(ctx).InsertOne(userEvent)
 }
 
 // Update イベントの編集
-func Update(ctx context.Context, txTime time.Time, eventID string, name string, organization string, tempMemberList []*TempMemberInfo) int32 {
+func Update(ctx context.Context, txTime time.Time, eventID string, name string, organization string) int32 {
 
 	query := bson.M{FEventID: eventID}
 
@@ -88,11 +68,6 @@ func Update(ctx context.Context, txTime time.Time, eventID string, name string, 
 			fName:         name,
 			fOrganization: organization,
 			fUpdateTime:   txTime,
-		},
-		"$addToSet": bson.M{
-			FTempMemberList: bson.M{
-				"$each": tempMemberList,
-			},
 		},
 	}
 
