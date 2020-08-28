55ef88725f89e0d4889831ac296b01a7c1395d59
sunjin <yun_sunjin@cocone.co.jp>
2020-08-28 09:16:54 +0900

    toastr

--- ap/asset/static/js/toast.js
+++ ap/asset/static/js/toast.js
@@ -1,5 +1,4 @@
 'use strict';
-import miniToastr from './mini-toastr.js';
 
 window.Alma = window.Alma || {};
 (function(_Alma) {
@@ -9,19 +8,155 @@ window.Alma = window.Alma || {};
         // constructor
         constructor() {
 
+            const ERROR = 'error';
+            const WARN = 'warn';
+            const SUCCESS = 'success';
+            const INFO = 'info';
+
             // const
             this.EMPTY_STRING = '';
+            this.LIB_NAME = 'mini-toastr';
+            this.ERROR = ERROR;
+            this.WARN = WARN;
+            this.SUCCESS = SUCCESS;
+            this.INFO = INFO;
+            this.CONTAINER_CLASS = this.LIB_NAME;
+            this.NOTIFICATION_CLASS = `${this.LIB_NAME}__notification`;
+            this.TITLE_CLASS = `${this.LIB_NAME}-notification__title`;
+            this.ICON_CLASS = `${this.LIB_NAME}-notification__icon`;
+            this.MESSAGE_CLASS = `${this.LIB_NAME}-notification__message`;
+            this.ERROR_CLASS = `-${this.ERROR}`;
+            this.WARN_CLASS = `-${this.WARN}`;
+            this.SUCCESS_CLASS = `-${this.SUCCESS}`;
+            this.INFO_CLASS = `-${this.INFO}`;
+            this.DEFAULT_TIMEOUT = 3000;
+
+            this.isInitialised = false;
+
+            // config
+            this.config = {
+                types: { ERROR, WARN, SUCCESS, INFO },
+                animation: this.fadeOut,
+                timeout: this.DEFAULT_TIMEOUT,
+                icons: {},
+                appendTarget: document.body,
+                node: this.makeNode(),
+                allowHtml: false,
+                style: {
+                    [`.${this.CONTAINER_CLASS}`]: {
+                        position: 'fixed',
+                        'z-index': 99999,
+                        right: '12px',
+                        top: '12px'
+                    },
+                    [`.${this.NOTIFICATION_CLASS}`]: {
+                        cursor: 'pointer',
+                        padding: '12px 18px',
+                        margin: '0 0 6px 0',
+                        'background-color': '#000',
+                        opacity: 0.8,
+                        color: '#fff',
+                        'border-radius': '3px',
+                        'box-shadow': '#3c3b3b 0 0 12px',
+                        width: '300px',
+                        [`&.${ERROR_CLASS}`]: {
+                            'background-color': '#D5122B'
+                        },
+                        [`&.${WARN_CLASS}`]: {
+                            'background-color': '#F5AA1E'
+                        },
+                        [`&.${SUCCESS_CLASS}`]: {
+                            'background-color': '#7AC13E'
+                        },
+                        [`&.${INFO_CLASS}`]: {
+                            'background-color': '#4196E1'
+                        },
+                        '&:hover': {
+                            opacity: 1,
+                            'box-shadow': '#000 0 0 12px'
+                        }
+                    },
+                    [`.${this.TITLE_CLASS}`]: {
+                        'font-weight': '500'
+                    },
+                    [`.${this.MESSAGE_CLASS}`]: {
+                        display: 'inline-block',
+                        'vertical-align': 'middle',
+                        width: '240px',
+                        padding: '0 12px'
+                    }
+                }
+            };
+
+            // init
+            this.init();
 
         }
 
+
+
         init(aConfig) {
             const newConfig = {};
-            Object.assign(newConfig, config);
+            Object.assign(newConfig, this.config);
             Object.assign(newConfig, aConfig);
             this.config = newConfig;
 
             const cssStr = this.makeCss(newConfig.style);
+            this.appendStyle(cssStr);
 
+            newConfig.node.id = this.CONTAINER_CLASS;
+            newConfig.node.className = this.CONTAINER_CLASS;
+            newConfig.appendTarget.appendChild(newConfig.node);
+
+            Object.keys(newConfig.types).forEach(v => {
+                this[newConfig.types[v]] = function(message, title, timeout, cb, config) {
+                    this.showMessage(message, title, newConfig.types[v], timeout, cb, config);
+                    return this;
+                }.bind(this);
+            });
+
+            this.isInitialised = true;
+
+            return this;
+
+        }
+
+        setIcon(type, nodeType = 'i', attrs = []) {
+            attrs.class = attrs.class ? attrs.class + ' ' + this.ICON_CLASS : this.ICON_CLASS;
+            this.config.icons[type] = {nodeType, attrs};
+        }
+
+        showMessage(message, title, type, timeout, cb, overrideConf) {
+
+            const config = {};
+            Object.assign(config, this.config);
+            Object.assign(config, overrideConf);
+
+            const notificationElem = this.makeNode();
+            notificationElem.className = `${this.NOTIFICATION_CLASS} ${this.getTypeClass(type)}`;
+
+            notificationElem.onclick = function() {
+                config.animation(notificationElem, null);
+            };
+
+            if (title) {
+                this.addElem(notificationElem, title, this.TITLE_CLASS, config);
+            }
+            if (config.icons[type]) {
+                this.createIcon(notificationElem, type, config);
+            }
+            if (message) {
+                this.addElem(notificationElem, message, this.MESSAGE_CLASS, config);
+            }
+
+            config.node.insertBefore(notificationElem, config.node.firstChild);
+            setTimeout(() => config.animation(notificationElem, cb), timeout || config.timeout);
+
+            if (cb) {
+                cb();
+            }
+
+            return this;
         }
 
         makeCss(obj) {
@@ -59,9 +194,75 @@ window.Alma = window.Alma || {};
             return into;
         }
 
-        appendStyle(css) {
-            let head = document.head || document.getElementsBy
+        makeNode(type = 'div') {
+            return document.createElement(type);
         }
+
+        createIcon(node, type, config) {
+            const iconNode = this.makeNode(config.icons[type].nodeType);
+            const attrs = config.icons[type].attrs;
+
+            for (const k in attrs) {
+                if (attrs.hasOwnProperty(k)) {
+                    iconNode.setAttribute(k, attrs[k]);
+                }
+            }
+
+            node.appendChild(iconNode);
+        }
+
+        addElem(node, text, className, config) {
+            const elem = this.makeNode();
+            elem.className = className;
+            if (config.allowHtml) {
+                elem.innerHTML = text;
+            } else {
+                elem.appendChild(document.createTextNode(text));
+            }
+            node.appendChild(elem);
+        }
+
+        getTypeClass(type) {
+            if (type === SUCCESS) return SUCCESS_CLASS;
+            if (type === WARN) return WARN_CLASS;
+            if (type === ERROR) return ERROR_CLASS;
+            if (type === INFO) return INFO_CLASS;
+
+            return EMPTY_STRING;
+        }
+
+        appendStyle(css) {
+            let head = document.head || document.getElementsByTagName('head')[0];
+            let styleElem = this.makeNode('style');
+            styleElem.id = `${this.LIB_NAME}-style`;
+            styleElem.type = 'text/css';
+
+            if (styleElem.styleSheet) {
+                styleElem.styleSheet.cssText = css;
+            } else {
+                styleElem.appendChild(document.createTextNode(css));
+            }
+
+            head.appendChild(styleElem);
+        }
+
+        fadeOut(element, cb) {
+            if (element.style.opacity && element.style.opacity > 0.05) {
+                element.style.opacity = element.style.opacity - 0.05;
+            } else if (element.style.opacity && element.style.opacity <= 0.1) {
+                if (element.parenetNode) {
+                    element.parentNode.removeChild(element);
+                    if (cb) {
+                        cb();
+                    }
+                }
+            } else {
+                element.style.opacity = 0.9;
+            }
+
+            setTimeout(() => this.fadeOut.apply(this, [element, cb]), 1000 / 30);
+        }
+
 
 
 
