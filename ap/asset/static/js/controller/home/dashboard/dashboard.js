1bc9016ecbc247fdf062ce8110eb3c89fd5a5391
sunjin <yun_sunjin@cocone.co.jp>
2020-08-28 17:46:36 +0900

    member info page

--- ap/asset/static/js/controller/home/dashboard/dashboard.js
+++ ap/asset/static/js/controller/home/dashboard/dashboard.js
@@ -5,20 +5,24 @@ class Dashboard {
 
         // getEl
         this.eventInfoPanelEl = document.querySelector('.js-event-info');
+        this.memberInfoPanelEl = document.querySelector('.js-member-info');
 
         this.goEventInfoPage = this.goEventInfoPage.bind(this);
+        this.goMemberInfoPage = this.goMemberInfoPage.bind(this);
 
         this.addEventListener();
     }
 
     addEventListener() {
-
         this.eventInfoPanelEl.addEventListener('click', this.goEventInfoPage);
-
+        this.memberInfoPanelEl.addEventListener('click', this.goMemberInfoPage);
     }
 
     goEventInfoPage() {
         window.Alma.location.href(window.Alma.location.event_info);
+    }
+    goMemberInfoPage() {
+        window.Alma.location.href(window.Alma.location.member_info);
     }
 }
 
--- ap/src/infrastructure/grpc/proto/common/common.pb.go
+++ ap/src/infrastructure/grpc/proto/common/common.pb.go
@@ -165,6 +165,53 @@ func (x *EventRequest) GetEvent() string {
 	return ""
 }
 
+type EventNameReply struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	EventName string `protobuf:"bytes,1,opt,name=event_name,json=eventName,proto3" json:"event_name,omitempty"`
+}
+
+func (x *EventNameReply) Reset() {
+	*x = EventNameReply{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_common_common_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *EventNameReply) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*EventNameReply) ProtoMessage() {}
+
+func (x *EventNameReply) ProtoReflect() protoreflect.Message {
+	mi := &file_common_common_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use EventNameReply.ProtoReflect.Descriptor instead.
+func (*EventNameReply) Descriptor() ([]byte, []int) {
+	return file_common_common_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *EventNameReply) GetEventName() string {
+	if x != nil {
+		return x.EventName
+	}
+	return ""
+}
+
 type EmailRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -176,7 +223,7 @@ type EmailRequest struct {
 func (x *EmailRequest) Reset() {
 	*x = EmailRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_common_common_proto_msgTypes[3]
+		mi := &file_common_common_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -189,7 +236,7 @@ func (x *EmailRequest) String() string {
 func (*EmailRequest) ProtoMessage() {}
 
 func (x *EmailRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_common_common_proto_msgTypes[3]
+	mi := &file_common_common_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -202,7 +249,7 @@ func (x *EmailRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use EmailRequest.ProtoReflect.Descriptor instead.
 func (*EmailRequest) Descriptor() ([]byte, []int) {
-	return file_common_common_proto_rawDescGZIP(), []int{3}
+	return file_common_common_proto_rawDescGZIP(), []int{4}
 }
 
 func (x *EmailRequest) GetEmail() string {
@@ -223,13 +270,16 @@ var file_common_common_proto_rawDesc = []byte{
 	0x01, 0x28, 0x09, 0x52, 0x04, 0x65, 0x6d, 0x73, 0x67, 0x22, 0x24, 0x0a, 0x0c, 0x45, 0x76, 0x65,
 	0x6e, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x76, 0x65,
 	0x6e, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x22,
-	0x24, 0x0a, 0x0c, 0x45, 0x6d, 0x61, 0x69, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
-	0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
-	0x65, 0x6d, 0x61, 0x69, 0x6c, 0x42, 0x35, 0x5a, 0x33, 0x61, 0x6c, 0x6d, 0x61, 0x2d, 0x73, 0x65,
-	0x72, 0x76, 0x65, 0x72, 0x2f, 0x61, 0x70, 0x2f, 0x73, 0x72, 0x63, 0x2f, 0x69, 0x6e, 0x66, 0x72,
-	0x61, 0x73, 0x74, 0x72, 0x75, 0x63, 0x74, 0x75, 0x72, 0x65, 0x2f, 0x67, 0x72, 0x70, 0x63, 0x2f,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x2f, 0x0a, 0x0e, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x4e, 0x61, 0x6d, 0x65, 0x52, 0x65, 0x70, 0x6c,
+	0x79, 0x12, 0x1d, 0x0a, 0x0a, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x4e, 0x61, 0x6d, 0x65,
+	0x22, 0x24, 0x0a, 0x0c, 0x45, 0x6d, 0x61, 0x69, 0x6c, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x12, 0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x42, 0x35, 0x5a, 0x33, 0x61, 0x6c, 0x6d, 0x61, 0x2d, 0x73,
+	0x65, 0x72, 0x76, 0x65, 0x72, 0x2f, 0x61, 0x70, 0x2f, 0x73, 0x72, 0x63, 0x2f, 0x69, 0x6e, 0x66,
+	0x72, 0x61, 0x73, 0x74, 0x72, 0x75, 0x63, 0x74, 0x75, 0x72, 0x65, 0x2f, 0x67, 0x72, 0x70, 0x63,
+	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -244,12 +294,13 @@ func file_common_common_proto_rawDescGZIP() []byte {
 	return file_common_common_proto_rawDescData
 }
 
-var file_common_common_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_common_common_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_common_common_proto_goTypes = []interface{}{
-	(*Empty)(nil),        // 0: common.Empty
-	(*Error)(nil),        // 1: common.Error
-	(*EventRequest)(nil), // 2: common.EventRequest
-	(*EmailRequest)(nil), // 3: common.EmailRequest
+	(*Empty)(nil),          // 0: common.Empty
+	(*Error)(nil),          // 1: common.Error
+	(*EventRequest)(nil),   // 2: common.EventRequest
+	(*EventNameReply)(nil), // 3: common.EventNameReply
+	(*EmailRequest)(nil),   // 4: common.EmailRequest
 }
 var file_common_common_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -302,6 +353,18 @@ func file_common_common_proto_init() {
 			}
 		}
 		file_common_common_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*EventNameReply); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_common_common_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*EmailRequest); i {
 			case 0:
 				return &v.state
@@ -320,7 +383,7 @@ func file_common_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
