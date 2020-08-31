0191f4a85bff0aaf91be7ba50547b22a4df33810
sunjin <yun_sunjin@cocone.co.jp>
2020-08-31 23:06:41 +0900

    中断

--- ap/asset/static/js/controller/member/member.js
+++ ap/asset/static/js/controller/member/member.js
@@ -1,1 +1,30 @@
-console.log("memberrrrrrrrrrr");
+'use strict';
+
+class MemberInfo {
+    constructor() {
+        // getEL
+        this.memberTableRowElList = document.querySelectorAll('.js-member-table-row');
+
+        // bind
+        this.checkRow = this.checkRow.bind(this);
+
+        this.addEventListener();
+    }
+
+    addEventListener() {
+
+        const me = this;
+        this.memberTableRowElList.forEach(function(elem) {
+            elem.addEventListener('click', me.checkRow);
+        })
+
+    }
+
+    // checkRow そのテーブルにRowを追加する
+    checkRow(elem) {
+        let checkBoxEl = elem.currentTarget.children[0].children[0];
+        checkBoxEl.checked = !checkBoxEl.checked;
+    }
+}
+
+new MemberInfo();
