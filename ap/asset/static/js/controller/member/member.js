42803d510c27de916f00abbdc6c7fd779f1fe1a0
sunjin <yun_sunjin@cocone.co.jp>
2020-09-03 09:11:21 +0900

    check box event

--- ap/asset/static/js/controller/member/member.js
+++ ap/asset/static/js/controller/member/member.js
@@ -4,20 +4,26 @@ class MemberInfo {
     constructor() {
         // getEL
         this.memberTableRowElList = document.querySelectorAll('.js-member-table-row');
+        this.memberTableHeadCheckboxEl = document.querySelector('.js-member-table-head-checkbox');
+        
 
         // bind
         this.checkRow = this.checkRow.bind(this);
+        this.checkAllRow = this.checkAllRow.bind(this);
+        this.checkAll = this.checkAll.bind(this);
+        this.uncheckAll = this.uncheckAll.bind(this);
+
 
         this.addEventListener();
     }
 
     addEventListener() {
-
         const me = this;
         this.memberTableRowElList.forEach(function(elem) {
             elem.addEventListener('click', me.checkRow);
-        })
+        });
 
+        this.memberTableHeadCheckboxEl.addEventListener('click', this.checkAllRow);
     }
 
     // checkRow そのテーブルにRowを追加する
@@ -25,6 +31,32 @@ class MemberInfo {
         let checkBoxEl = elem.currentTarget.children[0].children[0];
         checkBoxEl.checked = !checkBoxEl.checked;
     }
+
+    // checkAllRow 
+    checkAllRow() {
+    
+        if (this.memberTableHeadCheckboxEl.checked) {
+            this.checkAll();
+        } else {
+            this.uncheckAll();
+        }
+    }
+
+    // checkAll すべてのRowをcheckする
+    checkAll() {
+        this.memberTableRowElList.forEach(function(elem) {
+            elem.children[0].children[0].checked = true;
+        })
+    }
+
+    // uncheckAll すべてのRowのcheckを外す
+    uncheckAll() {
+        this.memberTableRowElList.forEach(function(elem) {
+            elem.children[0].children[0].checked = false;
+        })
+    }
+
+    
 }
 
 new MemberInfo();
