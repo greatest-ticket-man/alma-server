fb2062176c8d34e62f92633c717d2c299985de54
sunjin <yun_sunjin@cocone.co.jp>
2020-09-03 10:02:04 +0900

    update create-proto

--- ap/create-proto.sh
+++ ap/create-proto.sh
@@ -20,3 +20,12 @@ DIR = `pwd`
 cd ../..
 OUT_DIR=`pwd`
 
+# clear
+rm -rf $DIR/src/infrastructure/grpc/proto
+mkdir -p $DIR/src/infrastructure/grpc/proto
+
+export PATH="$PATH:$HOME/go/bin"
+
+cd $DIR/proto
+
+find . -type d -name vendor -prune -o -type f -name "*.proto" -exec protoc --proto_path=. -Ivendor/protobuf/src --go_out=plugins=grpc:${OUT_DIR} {}\;
