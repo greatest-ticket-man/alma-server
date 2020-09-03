855bdf65df17b4804f502f8d5fa50ae70c3344c2
sunjin <yun_sunjin@cocone.co.jp>
2020-09-03 09:54:33 +0900

    中断

--- ap/create-proto.sh
+++ ap/create-proto.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
-DIR=`pwd`
+# DIR=`pwd`
+
+# cd ../..
+# OUT_DIR=`pwd`
+
+
+
+# # clear
+# rm -rf $DIR/src/infrastructure/grpc/proto
+# mkdir -p $DIR/src/infrastructure/grpc/proto
+
+# export PATH="$PATH:$HOME/go/bin"
+
+# cd $DIR/proto
+# find . -name "*.proto" -type f -exec protoc --proto_path . --go_out=plugins=grpc:$OUT_DIR {} \;
+
+DIR = `pwd`
 
 cd ../..
 OUT_DIR=`pwd`
 
-
-
-# clear
-rm -rf $DIR/src/infrastructure/grpc/proto
-mkdir -p $DIR/src/infrastructure/grpc/proto
-
-export PATH="$PATH:$HOME/go/bin"
-
-cd $DIR/proto
-find . -name "*.proto" -type f -exec protoc --proto_path . --go_out=plugins=grpc:$OUT_DIR {} \;
---
+++ ap/proto/work_log.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir vendor
+cd vendor
+git submodule add git@github.com:protocolbuffers/protobuf.git 
