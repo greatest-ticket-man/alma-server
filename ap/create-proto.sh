7cd2dfbf532bd804e69cac76485df43997a27a42
sunjin <yun_sunjin@cocone.co.jp>
2020-09-03 10:41:18 +0900

    format

--- ap/create-proto.sh
+++ ap/create-proto.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-# DIR=`pwd`
-
-# cd ../..
-# OUT_DIR=`pwd`
-
-
-
-# # clear
-# rm -rf $DIR/src/infrastructure/grpc/proto
-# mkdir -p $DIR/src/infrastructure/grpc/proto
-
-# export PATH="$PATH:$HOME/go/bin"
-
-# cd $DIR/proto
-# find . -name "*.proto" -type f -exec protoc --proto_path . --go_out=plugins=grpc:$OUT_DIR {} \;
 
 DIR=`pwd`
 
@@ -27,7 +12,4 @@ mkdir -p $DIR/src/infrastructure/grpc/proto
 export PATH="$PATH:$HOME/go/bin"
 
 cd $DIR/proto
-
-
 find . -type d -name vendor -prune -o -type f -name "*.proto" | xargs --replace=AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
-
