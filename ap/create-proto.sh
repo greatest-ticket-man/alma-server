d3574acd4d6877b058d2ce36b278a27750be787a
sunjin <yun_sunjin@cocone.co.jp>
2020-09-03 10:46:34 +0900

    a

--- ap/create-proto.sh
+++ ap/create-proto.sh
@@ -12,4 +12,5 @@ mkdir -p $DIR/src/infrastructure/grpc/proto
 export PATH="$PATH:$HOME/go/bin"
 
 cd $DIR/proto
+find . -type d -name vendor -prune -o -type f -name "*.proto" | xargs echo # ./vendorも読み込まれてるから変なメッセージでるみたい、どうにかしたい
 find . -type d -name vendor -prune -o -type f -name "*.proto" | xargs --replace=AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
