54297512c223bee218d9e6f3eadd997722bf130e
sunjin <yun_sunjin@cocone.co.jp>
2020-09-06 08:14:25 +0900

    無限ループが発生する場合はPanicする

--- ap/src/domain/menu/MenuService/MenuService.go
+++ ap/src/domain/menu/MenuService/MenuService.go
@@ -1,18 +1,31 @@
 package MenuService
 
 import (
+	"alma-server/ap/src/common/error/chk"
 	"alma-server/ap/src/domain/menu/MenuComponent"
 	"alma-server/ap/src/infrastructure/grpc/proto/menu"
 	"alma-server/ap/src/repository/master/menu/MstMenuRepository"
+	"errors"
+	"time"
 )
 
-// GetMenu .
+// GetMenu メニューを取得する
+// 無限ループが発生する可能性があるので、取得に3秒以上かかった場合はPanicする
 func GetMenu(menuID string) *menu.MenuInfo {
 	mstMenuMap := MstMenuRepository.GetMap()
 
-	// TODO 無限ループを感知するためにchanを使う
+	ch := make(chan *menu.MenuInfo)
+	go func() {
+		ch <- getMenu(menuID, mstMenuMap)
+	}()
 
-	return getMenu(menuID, mstMenuMap)
+	select {
+	case result := <-ch:
+		return result
+	case <-time.After(time.Duration(3) * time.Second):
+		chk.SE(errors.New("Menuの取得時にタイムアウトしました"))
+		return nil
+	}
 }
 
 func getMenu(menuID string, mstMenuMap map[string]*MstMenuRepository.MstMenu) *menu.MenuInfo {
