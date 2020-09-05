b29fcf64dcc9e1266d2252657f54232c670a23ad
sunjin <yun_sunjin@cocone.co.jp>
2020-09-05 12:48:43 +0900

    templateにfuncを追加するok

--- ap/src/common/util/htmlutil/htmlutil.go
+++ ap/src/common/util/htmlutil/htmlutil.go
@@ -3,11 +3,26 @@ package htmlutil
 import (
 	"alma-server/ap/src/common/error/chk"
 	"alma-server/ap/src/common/projectpathap"
+	"alma-server/ap/src/common/util/dateutil"
 	"bytes"
 	"fmt"
 	"html/template"
 	"io"
+	"path/filepath"
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+// templateに追加したい、functionをココに追加する
+var funcMap = template.FuncMap{
+	"add": func(i1 int, i2 int) int {
+		return i1 + i2
+	},
+	"timestampToTime": func(ts *timestamppb.Timestamp) time.Time {
+		return dateutil.TimestampToTime(ts)
+	},
+}
 
 // Template .
 func Template(w io.Writer, path string, data interface{}) {
@@ -29,7 +44,11 @@ func CreateTemplateToString(path string, data interface{}) template.HTML {
 
 // getTemplateFromPath pathからtemplateを取得する
 func getTemplateFromPath(path string) *template.Template {
-	temp, err := template.ParseFiles(fmt.Sprintf("%s/asset%s", projectpathap.GetRoot(), path))
+
+	// Newにファイル名を与える必要がある
+	tname := filepath.Base(path)
+
+	temp, err := template.New(tname).Funcs(funcMap).ParseFiles(fmt.Sprintf("%s/asset%s", projectpathap.GetRoot(), path))
 	chk.SE(err)
 	return temp
 }
