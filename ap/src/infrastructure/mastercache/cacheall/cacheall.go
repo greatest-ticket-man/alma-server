d6645c65e3994e43e5c009a3161b7c0e0d690ad9
sunjin <yun_sunjin@cocone.co.jp>
2020-09-05 13:56:26 +0900

    a

--- ap/src/infrastructure/mastercache/cacheall/cacheall.go
+++ ap/src/infrastructure/mastercache/cacheall/cacheall.go
@@ -4,10 +4,12 @@ import (
 	"alma-server/ap/src/infrastructure/mastercache"
 	"alma-server/ap/src/repository/master/authority/MstEventAuthRepository"
 	"alma-server/ap/src/repository/master/authority/MstEventRoleRepository"
+	"alma-server/ap/src/repository/master/menu/MstMenuRepository"
 )
 
 // LoadMaster .
 func LoadMaster(dir string) {
 	MstEventAuthRepository.LoadCache(mastercache.Cache, dir)
 	MstEventRoleRepository.LoadCache(mastercache.Cache, dir)
+	MstMenuRepository.LoadCache(mastercache.Cache, dir)
 }
