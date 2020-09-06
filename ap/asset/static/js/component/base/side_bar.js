5263d3f147000da15e52ca1a7ce7aa36cebabb9c
sunjin <yun_sunjin@cocone.co.jp>
2020-09-06 10:42:43 +0900

    side menu

--- ap/asset/static/js/component/base/side_bar.js
+++ ap/asset/static/js/component/base/side_bar.js
@@ -1,31 +1,41 @@
 'use strict';
 
-console.log("hoge");
-
 class SideBar {
     constructor() {
 
         // getEl 
-        this.sideBarHeader = document.querySelector('.js-sidebar-header');
-        this.sideBarHeaderPath = document.querySelector('.js-sidebar-header-path');
+        this.sideBarHeaderEl = document.querySelector('.js-sidebar-header');
+        this.sideBarHeaderPathEl = document.querySelector('.js-sidebar-header-path');
+        this.sideBarMenuRowElList = document.querySelectorAll('.js-sidebar-menu-row');
 
-
-        this.goHeaderpath = this.goHeaderpath.bind(this);
+        // bind this
+        this.goHeaderPath = this.goHeaderPath.bind(this);
+        this.goPath = this.goPath.bind(this);
 
         this.addEventListener();
     }
 
     addEventListener() {
 
-        this.sideBarHeader.addEventListener('click', this.goHeaderpath);
+        this.sideBarHeaderEl.addEventListener('click', this.goHeaderPath);
+
+        const me = this;
+        this.sideBarMenuRowElList.forEach(function(elem) {
+            elem.addEventListener('click', me.goPath);
+        })
+
     }
 
-    goHeaderpath() {
-        let path =  this.sideBarHeaderPath.innerText;
+    goHeaderPath() {
+        let path =  this.sideBarHeaderPathEl.innerText;
         window.Alma.location.href(path);
     }
 
-
+    goPath(elem) {
+        let sideBarMenuRow = elem.currentTarget;
+        let path = sideBarMenuRow.querySelector('.js-sidebar-path').innerText;
+        window.Alma.location.href(path);
+    }
 }
 
 new SideBar();
