5c122718da98c1de8233a8257b98f33ba6df8cbf
sunjin <yun_sunjin@cocone.co.jp>
2020-09-06 10:33:55 +0900

    中断

--- ap/asset/static/js/component/base/side_bar.js
+++ ap/asset/static/js/component/base/side_bar.js
@@ -1,9 +1,31 @@
 'use strict';
 
+console.log("hoge");
+
 class SideBar {
     constructor() {
-        
+
+        // getEl 
+        this.sideBarHeader = document.querySelector('.js-sidebar-header');
+        this.sideBarHeaderPath = document.querySelector('.js-sidebar-header-path');
+
+
+        this.goHeaderpath = this.goHeaderpath.bind(this);
+
+        this.addEventListener();
     }
+
+    addEventListener() {
+
+        this.sideBarHeader.addEventListener('click', this.goHeaderpath);
+    }
+
+    goHeaderpath() {
+        let path =  this.sideBarHeaderPath.innerText;
+        window.Alma.location.href(path);
+    }
+
+
 }
 
 new SideBar();
