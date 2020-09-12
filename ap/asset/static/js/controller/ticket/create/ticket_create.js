4d00ab06af0c59e96b79839c7b5c84ead0b7a614
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-09-12 15:56:46 +0900

    button

--- ap/asset/static/js/controller/ticket/create/ticket_create.js
+++ ap/asset/static/js/controller/ticket/create/ticket_create.js
@@ -4,15 +4,27 @@ class TicketCreate {
     constructor() {
 
         this.ticketInfoBackButtonEl = document.querySelector('.js-ticket-info-back');
+        this.ticketCreateCancelButtonEl = document.querySelector('.js-ticket-create-cancel');
+        this.ticketCreateButtonEl = document.querySelector('.js-ticket-create');
 
         this.goTicketInfoPage = this.goTicketInfoPage.bind(this);
+        this.createTicket = this.createTicket.bind(this);
 
         this.addEventListener();
     }
 
     addEventListener() {
         this.ticketInfoBackButtonEl.addEventListener('click', this.goTicketInfoPage);
+        this.ticketCreateCancelButtonEl.addEventListener('click', this.goTicketInfoPage);
+        this.ticketCreateButtonEl.addEventListener('click', this.createTicket);
     }
+
+    // createTicket 
+    createTicket() {
+        // TODO チケットの作成
+        console.log("create ticket !! 未実装");
+    }
+
 
     // goTicketInfoPage
     goTicketInfoPage() {
