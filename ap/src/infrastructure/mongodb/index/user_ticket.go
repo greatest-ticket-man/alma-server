96455c2102e5eac4605a759b220d8daad8f07133
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-09-12 15:15:10 +0900

    ticket repository

--- ap/src/infrastructure/mongodb/index/user_ticket.go
+++ ap/src/infrastructure/mongodb/index/user_ticket.go
@@ -1,16 +1,23 @@
 package index
 
 import (
+	"alma-server/ap/src/repository/user/ticket/UserTicketRepository"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func init() {
-	userIndexMap["USER_TICKET"] = []mongo.IndexModel{
+
+	userIndexMap[UserTicketRepository.ThisCollectionName] = []mongo.IndexModel{
 		{
-			Keys:    bson.M{"_id": "hashed"},
-			Options: options.Index().SetBackground(true),
+			Keys: bson.D{
+				{Key: UserTicketRepository.FEventID, Value: 1},
+				{Key: UserTicketRepository.FTicketID, Value: 1},
+			},
+			Options: options.Index().SetBackground(true).SetUnique(true),
 		},
 	}
+
 }
