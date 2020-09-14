0b1b00c0c6214951ee6acfb7d78fb6b3767b17cf
sunjin <yun_sunjin@cocone.co.jp>
2020-09-14 14:23:30 +0900

    ticketID

--- ap/src/domain/ticket/TicketRpcService/TicketRpcService.go
+++ ap/src/domain/ticket/TicketRpcService/TicketRpcService.go
@@ -1,10 +1,31 @@
 package TicketRpcService
 
 import (
+	"alma-server/ap/src/common/util/jsonutil"
+	"alma-server/ap/src/domain/event/EventService"
+	"alma-server/ap/src/domain/ticket/TicketComponent"
+	"alma-server/ap/src/infrastructure/grpc/proto/ticket"
 	"alma-server/ap/src/repository/user/ticket/UserTicketRepository"
 	"context"
+	"log"
 	"time"
 )
+
+// UpdatePage チケットの編集画面
+func UpdatePage(ctx context.Context, mid string, txTime time.Time, eventID string, ticketID string) *ticket.UpdatePageReply {
+
+	// todo 権限
+	userTicket := UserTicketRepository.FindOne(ctx, eventID, ticketID)
+
+	log.Println("userTicket is ", jsonutil.Marshal(userTicket))
+
+	eventName := EventService.GetEventName(ctx, eventID)
+
+	return &ticket.UpdatePageReply{
+		EventName:  eventName,
+		TicketInfo: TicketComponent.CreateTicketInfo(userTicket),
+	}
+}
 
 // CreateTicket チケットの作成
 func CreateTicket(ctx context.Context, mid string, txTime time.Time, eventID string, ticketID string, ticketName string,
