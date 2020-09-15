b75c3d7fe563125cecdb3591405e06427ea0f23b
sunjin <yun_sunjin@cocone.co.jp>
2020-09-16 00:24:32 +0900

    change script

--- ap/release.sh
+++ ap/release.sh
@@ -14,9 +14,9 @@ scp -p ./ap www@221.170.118.102:/home/www
 scp -pr ./asset www@221.170.118.102:/home/www
 scp -pr ./config www@221.170.118.102:/home/www
 
-# start
-# ssh -t www@221.170.118.102 'sudo ./start-alma.sh'
-echo "直接Serverでstartは実行する必要があります"
+# start TODO logoutしても動き続けるScriptにする
+# ssh -t www@221.170.118.102 'nohop sudo ./start-alma.sh'
+
 
 # バイナリを削除
 rm ap
