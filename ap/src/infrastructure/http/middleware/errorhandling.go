2a4380ab3bfe07c3e39fbc00e8b43250ad0434fd
sunjin <yun_sunjin@cocone.co.jp>
2020-09-25 23:38:35 +0900

    505 error make

--- ap/src/infrastructure/http/middleware/errorhandling.go
+++ ap/src/infrastructure/http/middleware/errorhandling.go
@@ -3,31 +3,39 @@ package middleware
 import (
 	"alma-server/ap/src/common/error/almaerror"
 	"alma-server/ap/src/common/error/errmsg"
+	"alma-server/ap/src/common/logger"
 	"alma-server/ap/src/common/util/httputil/response"
-	"fmt"
-	"log"
+	"alma-server/ap/src/controller/common"
 	"net/http"
 
 	stripego "github.com/stripe/stripe-go/v71"
 )
-
-// errorhandling panicが発生した時にCatchするMiddleware
 
 // ErrorHandlingMiddleware panicが発生した時にCatchするMiddleware
 func ErrorHandlingMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 
 	defer func() {
 		if panicErr := recover(); panicErr != nil {
-			doServerErrorProcess(w, panicErr)
+			reason := logAndReason(w, panicErr)
+
+			if r.Method == "GET" { // GET requestの場合はUIを表示する
+				common.InternalServerErrorPageHTML(w, r, reason)
+			} else {
+				response.ERROR(w, reason)
+			}
 		}
 	}()
 
 	next(w, r)
 }
 
-// doServerErrorProcess
-// TODO Logを流すためのあれそれ
-func doServerErrorProcess(w http.ResponseWriter, err interface{}) {
+// getServerErrorProcess GETRequest時の、Error処理
+func getHandling(w http.ResponseWriter, r *http.Request, err interface{}) {
+	reason := logAndReason(w, err)
+	common.InternalServerErrorPageHTML(w, r, reason)
+}
+
+func logAndReason(w http.ResponseWriter, err interface{}) string {
 
 	var reason string
 	switch e := err.(type) {
@@ -35,38 +43,81 @@ func doServerErrorProcess(w http.ResponseWriter, err interface{}) {
 	case *almaerror.SystemError:
 		reason = "エラーが発生しました" // System的なErrorなのでViewには見せない
 
-		fmt.Printf("%+v", e.Err)
-
-		log.Printf("[SYSTEM ERROR] statuscode=%d msgcode=%s msg=%s err=%v", e.StatusCode, e.MessageCode,
+		logger.Infof("[SYSTEM ERROR] statuscode=%d msgcode=%s msg=%s err=%v", e.StatusCode, e.MessageCode,
 			errmsg.Get("ja", e.MessageCode, e.Params...),
 			e.Err,
 		)
 	case *almaerror.LogicError:
 		reason = errmsg.Get("ja", e.MessageCode, e.Params...)
-		log.Printf("[LOGIC ERROR] statuscode=%d msgcode=%s msg=%s", e.StatusCode, e.MessageCode, reason)
+		logger.Infof("[LOGIC ERROR] statuscode=%d msgcode=%s msg=%s", e.StatusCode, e.MessageCode, reason)
 	case *almaerror.BillingError:
 		// TODO
-		log.Println("Billing errorです", e)
+		// log.Println("Billing errorです", e)
 		if stripeErr, ok := err.(*stripego.Error); ok {
 
 			switch stripeErr.Code {
 			case stripego.ErrorCodeAPIKeyExpired:
 				// たくさん
 			}
-
 		}
 
 		reason = "課金Errorです"
 	case error:
-		log.Printf("Unknown error: %+v\n", e)
+		logger.Infof("Unknown error: %+v\n", e)
 		reason = "不明なエラーが発生しました"
 	default:
-		log.Println("到達不能Errorです")
+		logger.Infof("到達不能Errorです")
 		reason = "到達不能Errorが発生しました"
-
 	}
 
-	// response
-	response.ERROR(w, reason)
-
+	return reason
 }
+
+// func printAndReason() {
+
+// }
+
+// doServerErrorProcess
+// TODO Logを流すためのあれそれ
+// func doServerErrorProcess(w http.ResponseWriter, err interface{}) {
+
+// 	var reason string
+// 	switch e := err.(type) {
+
+// 	case *almaerror.SystemError:
+// 		reason = "エラーが発生しました" // System的なErrorなのでViewには見せない
+
+// 		fmt.Printf("%+v", e.Err)
+
+// 		log.Printf("[SYSTEM ERROR] statuscode=%d msgcode=%s msg=%s err=%v", e.StatusCode, e.MessageCode,
+// 			errmsg.Get("ja", e.MessageCode, e.Params...),
+// 			e.Err,
+// 		)
+// 	case *almaerror.LogicError:
+// 		reason = errmsg.Get("ja", e.MessageCode, e.Params...)
+// 		log.Printf("[LOGIC ERROR] statuscode=%d msgcode=%s msg=%s", e.StatusCode, e.MessageCode, reason)
+// 	case *almaerror.BillingError:
+// 		// TODO
+// 		log.Println("Billing errorです", e)
+// 		if stripeErr, ok := err.(*stripego.Error); ok {
+
+// 			switch stripeErr.Code {
+// 			case stripego.ErrorCodeAPIKeyExpired:
+// 				// たくさん
+// 			}
+// 		}
+
+// 		reason = "課金Errorです"
+// 	case error:
+// 		log.Printf("Unknown error: %+v\n", e)
+// 		reason = "不明なエラーが発生しました"
+// 	default:
+// 		log.Println("到達不能Errorです")
+// 		reason = "到達不能Errorが発生しました"
+
+// 	}
+
+// 	// response
+// 	response.ERROR(w, reason)
+
+// }
