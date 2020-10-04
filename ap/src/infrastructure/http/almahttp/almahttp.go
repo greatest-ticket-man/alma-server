91879fe73af6d78600482f48827079fd0d5661e9
sunjin <sunjin110@gmail.com>
2020-10-04 14:14:33 +0900

    予約作成画面

--- ap/src/infrastructure/http/almahttp/almahttp.go
+++ ap/src/infrastructure/http/almahttp/almahttp.go
@@ -75,6 +75,7 @@ func Router() *negroni.Negroni {
 	authRouter.HandleFunc("/member", member.PageHTML).Methods("GET")
 	authRouter.HandleFunc("/member/invite", memberinvite.PageHTML).Methods("GET")
 	authRouter.HandleFunc("/reserve", reserve.PageHTML).Methods("GET")
+	authRouter.HandleFunc("/reserve/create", reserve.CreatePageHTML).Methods("GET")
 	authRouter.HandleFunc("/ticket", ticket.PageHTML).Methods("GET")
 	authRouter.HandleFunc("/ticket/create", ticket.CreatePageHTML).Methods("GET")
 	authRouter.HandleFunc("/ticket/create", ticket.CreateTicket).Methods("POST")
