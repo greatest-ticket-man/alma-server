bfb3b35b204871db6dc61e1f2c9c2d77a494e061
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-10-07 15:29:57 +0900

    docker todo

---
+++ script/ap/infra/docker/makeImage.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# set -eu
+
+VERSION='0.0.1'
+workspce=`pwd`
+
+mkdir deploy 2> /dev/null
+
+cd ../../../../ap
+
+./build.sh
+
+cp ./alma-ap ${workspce}/deploy/
+cp -r ./config ${workspce}/deploy/
+
+cd $workspce
+
+# build image
+docker build --tag sunjin110/alma:${VERSION} .
+
+
+# build push
+docker push sunjin110/alma:${VERSION}
---
+++ script/ap/infra/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+VERSION="0.0.1"
+
+docker run -p 3000:3000 -it sunjin110/alma:${VERSION}
