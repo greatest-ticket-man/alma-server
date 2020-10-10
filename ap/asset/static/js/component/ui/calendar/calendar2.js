bd7a9d6f28611ce0c5a78cfd968c8dda160ad744
sunjin <sunjin110@gmail.com>
2020-10-10 14:29:57 +0900

    calendar todo

--- ap/asset/static/js/component/ui/calendar/calendar2.js
+++ ap/asset/static/js/component/ui/calendar/calendar2.js
@@ -1,1 +1,115 @@
-console.log("hello world");
+'use strict';
+
+class Calendar {
+    constructor() {
+
+        console.log("calendar...");
+
+        // valiable
+        this.date = new Date();
+        this.today = new Date(); // TODO select day
+        this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        // EL
+        this.dateTitleEl = document.querySelector('.js-calendar-date-title');
+        this.dateDescEl = document.querySelector('.js-calendar-date-desc');
+        this.daysEl = document.querySelector('.js-calendar-days');
+
+        // bind
+        this.renderCalendar = this.renderCalendar.bind(this);
+
+        this.renderCalendar();
+
+        this.addEventListener();
+
+    }
+
+    addEventListener() {
+
+    }
+
+    // renderCalendar カレンダーをレンダリングする
+    renderCalendar() {
+
+        // 日数を1日にする
+        this.date.setDate(1);
+
+        // 最終日
+        const lastDate = this.getLastDate();
+        const prevLastDate = this.getPrevLastDate();
+
+        const firstDayIndex = this.getFirstDayIndex();
+        const lastDayIndex = this.getLastDayIndex();
+
+        const nextDate = 7 - lastDayIndex - 1;
+
+        // title
+        this.dateTitleEl.innerHTML = this.months[this.date.getMonth()];
+        this.dateDescEl.innerHTML = this.today.toDateString();
+
+        // 前の月の日付
+        let days = '';
+        for (let i = firstDayIndex; i > 0; i--) {
+            days += `<div class="prev-date">${prevLastDate - i + 1}</div>`;
+        }
+
+        // 今月の日付
+        for (let i = 1; i <= lastDate; i++) {
+            if (i === this.today.getDate() 
+                && this.date.getMonth() === this.today.getMonth()) {
+                days += `<div class="today">${i}</div>`
+            } else {
+                days += `<div>${i}</div>`;
+            }
+        }
+
+        // 来月の日付
+        for (let i = 1; i <= nextDate; i++) {
+            days += `<div class"next-date></div>"`
+        }
+
+        this.daysEl.innerHTML = days;
+
+    }
+
+    getLastDate() {
+        let date = this.date;
+        return new Date(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            0,
+        ).getDate();
+    }
+
+    getPrevLastDate() {
+        let date = this.date;
+        return new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            0,
+        ).getDate();
+    }
+
+    // 最初の曜日のindexを取得する
+    getFirstDayIndex() {
+        return this.date.getDay();
+    }
+
+    // 最後の日の曜日indexを取得する
+    getLastDayIndex() {
+        let date = this.date;
+        return new Date(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            0,
+        ).getDay();
+    }
+
+    // 次の曜日のindexを取得する
+    prevNextDayIndex(dayIndex) {
+        return 7 - dayIndex - 1;
+    }
+
+}
+
+new Calendar();
