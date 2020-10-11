190ed3d6b391a8a29abe9880204b461ef4ccf71f
sunjin <sunjin110@gmail.com>
2020-10-11 18:26:42 +0900

    ticket adder

--- ap/asset/static/js/component/ticket/form.js
+++ ap/asset/static/js/component/ticket/form.js
@@ -8,8 +8,24 @@ class TicketForm {
         this.ticketNameEl = document.getElementById('js-ticket-name');
         this.ticketPriceEl = document.getElementById('js-ticket-price');
         this.ticketDescEl = document.getElementById('js-ticket-desc');
-        this.ticketStockEl = document.getElementById('js-ticket-stock');
-        this.ticketEventStartTimeEl = document.getElementById('js-ticket-event-start-time');
+
+        // this.ticketStockEl = document.getElementById('js-ticket-stock');
+        // this.ticketEventStartTimeEl = document.getElementById('js-ticket-event-start-time');
+
+        this.scheduleStockTableEl = document.querySelector('.js-schedule-stock-table-body');
+        this.scheduleStockTableAddButtonEl = document.querySelector('.js-schedule-stock-add-button');
+        this.scheduleStockRowTmpEl = document.querySelector('#js-schedule-stock-row-tmp');
+
+        // bind
+        this.addMultiStockTable = this.addMultiStockTable.bind(this);
+
+        this.addEventListener();
+
+    }
+
+    addEventListener() {
+
+        this.scheduleStockTableAddButtonEl.addEventListener('click', this.addMultiStockTable);
 
     }
 
@@ -25,21 +41,27 @@ class TicketForm {
         return Number(this.ticketPriceEl.value);
     }
 
-    getTicketStock() {
-        return Number(this.ticketStockEl.value);
+    // addMultiStockTable テーブルのrowを追加する
+    addMultiStockTable() {
+        const t = this.scheduleStockRowTmpEl;
+        const clone = t.content.cloneNode(true);
+        this.scheduleStockTableEl.appendChild(clone);
     }
 
-    getTicketDesc() {
-        return this.ticketDescEl.value;
-    }
+    // getTicketStock() {
+    //     return Number(this.ticketStockEl.value);
+    // }
 
-    /**
-     * 2020-09-18T17:05
-     */
-    getTicketEventStartTime() {
+    // getTicketDesc() {
+    //     return this.ticketDescEl.value;
+    // }
 
-        return new Date(this.ticketEventStartTimeEl.value);
-    }
+    // /**
+    //  * 2020-09-18T17:05
+    //  */
+    // getTicketEventStartTime() {
+    //     return new Date(this.ticketEventStartTimeEl.value);
+    // }
 
 }
 
