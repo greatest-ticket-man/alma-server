95945de3df325c71c162ec05496e5cde4f9cffc5
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-10-14 11:04:56 +0900

    static check

--- ap/src/common/util/reflectutil/reflectutil.go
+++ ap/src/common/util/reflectutil/reflectutil.go
@@ -13,3 +13,29 @@ func CreateSlice(reflectType reflect.Type) reflect.Value {
 	slicePtr := reflect.ValueOf(reflectionValue.Interface())
 	return slicePtr.Elem()
 }
+
+// IsPointer 指定したReflectTypeがPointerかどうかを判定
+// true: pointer, false: 値
+func IsPointer(reflectType reflect.Type) bool {
+	return reflectType.Kind() == reflect.Ptr
+}
+
+// IsNil 確実にnilを検出する
+// golangのnilは型を持つため、interface{}でnilを受け取ったときに、キャストしないとnil判定ができない
+// そんな時のために、nilかどうかの判定ができるようにしたもの
+// structの場合は、初期値があるのでnilではない判定にします
+func IsNil(x interface{}) bool {
+
+	// reflectはコストが高いので、先にreflectを使わない判定をします
+	if x == nil {
+		return true
+	}
+
+	// structの場合は、reflect.ValueOf(x).IsNil()でpanicするため先に判定
+	if !IsPointer(reflect.TypeOf(x)) {
+		// structなので、nilではありません
+		return false
+	}
+
+	return reflect.ValueOf(x).IsNil()
+}
