4813d8c2ebde2aabe4a82ad5ce937ee1b7186ec8
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-10-15 15:52:34 +0900

    update add schedule and stock

--- ap/src/common/util/dateutil/dateutil.go
+++ ap/src/common/util/dateutil/dateutil.go
@@ -14,16 +14,21 @@ const (
 	// YyyyMm .
 	YyyyMm = "200601"
 
-	// YyyyMmDdSs .
-	YyyyMmDdSs = "2006/01/02 15:04:05"
+	// YYYYMMDDhhmmss .
+	YYYYMMDDhhmmss = "2006/01/02 15:04:05"
 
-	// YyyyMmDdTHhSs Formで取得した時の形式
-	YyyyMmDdTHhSs = "2006-01-02T15:04"
+	// YYYYMMDDThhmm Formで取得した時の形式
+	YYYYMMDDThhmm = "2006-01-02T15:04"
 )
 
-// FormatYyyyMmDdSs 日付文字列を返す (例：2019-12-07 11:34:53）
-func FormatYyyyMmDdSs(t time.Time) string {
-	return t.Format(YyyyMmDdSs)
+// FormatYYYYMMDDhhmmss 日付文字列を返す (例：2019-12-07 11:34:53）
+func FormatYYYYMMDDhhmmss(t time.Time) string {
+	return t.Format(YYYYMMDDhhmmss)
+}
+
+// FormatYYYYMMDDhhmm 日付文字列を返す (例: 2020-10-15T15:40)
+func FormatYYYYMMDDhhmm(t time.Time) string {
+	return t.Format(YYYYMMDDThhmm)
 }
 
 // TimeToTimestamp golangのtimeからTimestampを作成する
@@ -45,5 +50,5 @@ func ParseFormatStringToTime(format string, strTime string) time.Time {
 
 // ParseFormStrToTime フォームでのフォーマットを変更して、time.Timeに変換する
 func ParseFormStrToTime(strTime string) time.Time {
-	return ParseFormatStringToTime(YyyyMmDdTHhSs, strTime)
+	return ParseFormatStringToTime(YYYYMMDDThhmm, strTime)
 }
