2bdb541d2c3ab4bbe1690a3346b9abe54ec80df3
sunjin <sunjin110@gmail.com>
2020-10-18 13:27:55 +0900

    server side

--- ap/src/domain/reserve/ReserveRpcService/ReserveRpcService.go
+++ ap/src/domain/reserve/ReserveRpcService/ReserveRpcService.go
@@ -5,8 +5,10 @@ import (
 	"alma-server/ap/src/common/error/errmsg"
 	"alma-server/ap/src/domain/event/EventService"
 	"alma-server/ap/src/domain/reserve/ReserveComponent"
+	"alma-server/ap/src/domain/ticket/TicketComponent"
 	"alma-server/ap/src/infrastructure/grpc/proto/reserve"
 	"alma-server/ap/src/repository/user/reserve/UserReserveRepository"
+	"alma-server/ap/src/repository/user/ticket/UserTicketRepository"
 	"context"
 	"time"
 )
@@ -29,5 +31,24 @@ func Page(ctx context.Context, mid string, txTime time.Time, eventID string) *re
 		EventId:         eventID,
 		EventName:       userEvent.Name,
 		ReserveInfoList: reserveInfoList,
+	}
+}
+
+// CreatePage 予約作成画面を取得する
+func CreatePage(ctx context.Context, mid string, txTime time.Time, eventID string) *reserve.CreatePageReply {
+
+	userEvent := EventService.GetEvent(ctx, eventID)
+	if userEvent == nil {
+		// イベントが存在しません
+		chk.LE(errmsg.EventNotFound)
+	}
+
+	// GetTicketAll
+	userTicketList := UserTicketRepository.Find(ctx, eventID)
+
+	return &reserve.CreatePageReply{
+		EventId:    eventID,
+		EventName:  userEvent.Name,
+		TicketList: TicketComponent.CreateTicketInfoList(userTicketList),
 	}
 }
