3a298a82c8f9786b462ba8bbe288f0d4e2a4189f
sunjin <yun_sunjin@cocone.co.jp>
2020-10-25 16:00:41 +0900

    sale form

--- ap/src/controller/sales/sales.go
+++ ap/src/controller/sales/sales.go
@@ -1,8 +1,11 @@
 package sales
 
 import (
+	"alma-server/ap/src/common/almactx"
 	"alma-server/ap/src/common/util/httputil/param"
 	"alma-server/ap/src/common/util/httputil/response"
+	"alma-server/ap/src/domain/event/EventRpcService"
+	"alma-server/ap/src/domain/menu/MenuService"
 	"alma-server/ap/src/infrastructure/grpc/proto/common"
 	"net/http"
 )
@@ -10,21 +13,28 @@ import (
 // PageHTML 売上画面のトップ
 func PageHTML(w http.ResponseWriter, r *http.Request) {
 
+	ctx := r.Context()
+	mid := almactx.GetMid(ctx)
+
 	// param
-	_ = &common.EventRequest{
+	req := &common.EventRequest{
 		Event: param.Value(r, "event"),
 	}
+
+	result := EventRpcService.GetEvent(ctx, mid, req.Event)
 
 	response.BaseHTML(
 		w,
 		"売上状況",
-		"/template/controller/sales/head.html",
+		"",
 		map[string]interface{}{},
 		"/template/controller/sales/sales.html",
 		map[string]interface{}{},
 		[]string{},
 		[]string{},
-		"eventNameTODO",
-		nil,
+		result.EventName,
+		MenuService.GetMenu("sales_top", "sales"),
+		// nil,
+		// MenuService.GetMenu("reserve_top", "reserve"),
 	)
 }
