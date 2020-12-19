a449bdfa9da63b4f2ee3310ac5d840d91c11b716
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-12-19 18:17:40 +0900

    a

--- elda/src/ctrl/reservectrl/reservectrl.go
+++ elda/src/ctrl/reservectrl/reservectrl.go
@@ -7,6 +7,12 @@ import (
 
 // PageHTML .
 func PageHTML(w http.ResponseWriter, r *http.Request) {
+
+	// TODO EventIDがない場合は、Error
+
+	// TODO そのイベントの情報公開日より前の場合はError
+
+	// TODO 大丈夫であれば、チケットとかの情報を表示してあげる
 	// 予約画面
 	response.HTML(w, "/template/ctrl/reserve/reserve.html", map[string]interface{}{})
 }
